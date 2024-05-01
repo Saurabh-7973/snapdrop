3f498495c30457ff9e569be0dce3af79306f0a93
Saurabh7973 <saurabhupadhyay7973@gmail.com>
2024-05-02 01:23:15 +0530

    latest Snapdrop 1.0.2 -v1 02052024

--- lib/services/permission_provider.dart
+++ lib/services/permission_provider.dart
@@ -1,9 +1,24 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class PermissionProviderServices {
+  final deviceInfoPlugin = DeviceInfoPlugin();
+
   Future<bool> requestMediaAccessPermission() async {
-    PermissionStatus status = await Permission.photos.request();
+    final deviceInfo = await deviceInfoPlugin.deviceInfo;
+    log(deviceInfo.data.toString());
+    final version = deviceInfo.data['version']['release'];
+    PermissionStatus status;
+
+    if (Platform.isAndroid == true && int.parse(version) > 12) {
+      status = await Permission.photos.request();
+    } else {
+      status = await Permission.storage.request();
+    }
     if (status.isGranted) {
       return true;
     } else {
